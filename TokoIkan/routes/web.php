e89dae1968c29befa59cc7e2811c418a18e7a3e8
Salmoon16 <alfarizijunior123@gmail.com>
2025-02-08 14:47:42 +0700

    Salman Tambahh buanyak

--- TokoIkan/routes/web.php
+++ TokoIkan/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('pages.dashboard');
+});
+
+Route::get('/', function () {
+    return view('pages.login');
+});
