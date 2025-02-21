3cc6fc42af886cb8615f5dc2f930083b74a3761a
Salmoon16 <alfarizijunior123@gmail.com>
2025-02-21 10:13:03 +0700

    regis, forget password,

--- fillament-port/app/Providers/Filament/AdminPanelProvider.php
+++ fillament-port/app/Providers/Filament/AdminPanelProvider.php
@@ -27,9 +27,9 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            //->registration(0)
-            ->emailVerification()
+            ->registration()
             ->passwordReset()
+            ->emailVerification()
             ->colors([
                 'primary' => Color::Amber,
             ])
@@ -38,7 +38,7 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
-            // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             // ->widgets([
             //     Widgets\AccountWidget::class,
             //     Widgets\FilamentInfoWidget::class,
